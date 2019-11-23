6ed7d21eb8b4a7706f96cf798d97fb639c9cf50a
MariaPinto <38145387+MariaPinto@users.noreply.github.com>
2019-11-23 12:15:52 -0500

    Add files via upload

---
+++ login/src/main/java/com/gpch/login/repository/ProductosRepository.java
@@ -0,0 +1,9 @@
+package com.gpch.login.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.gpch.login.model.Productos;
+
+public interface ProductosRepository  extends JpaRepository<Productos, Long>{
+
+}
